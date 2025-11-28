b2b5ceae83ddc8be9f5667ee89f1f2f067655285
Abutarik <abdotmyt@email.com>
2025-11-28 23:14:06 +0200

    Added Exercises1 For Day 3

---
+++ Day3/Exercises1.js
@@ -0,0 +1,72 @@
+// 1
+let FirstName = 'ali' ;
+let LastName = 'ohaio' ;
+let country = 'Libya' ;
+let city = 'Tripoli' ;
+let age = 25 ;
+let isMarried = false ;
+const now = new Date() ;
+
+console.log(typeof FirstName)
+console.log(typeof LastName)
+console.log(typeof country)
+console.log(typeof city)
+console.log(typeof age)
+console.log(typeof isMarried)
+
+// 2
+console.log('10' == 10)
+
+// 3
+console.log(parseInt(9.8)==10)
+
+// 4
+ // i 
+console.log(4>3) 
+console.log(3 <= 5) 
+console.log(6>2) 
+ //ii
+console.log(4<3) 
+console.log(5 <= 3) 
+console.log(6<2)
+
+// 5
+console.log(true, true, false, false, true, true, false, false, false, true, false )
+
+// Find the length of python and jargon and make a falsy comparison statement.
+let python = `python` ;
+let jargon = `jargon` ;
+console.log (python.length != jargon.length) 
+console.log (python.length > jargon.length) 
+console.log (python.length < jargon.length) 
+console.log (python.length !== jargon.length) // didn`t understand this question 
+
+// 6
+ //i The result is true because both sides are true.
+ //ii The result is false because one of the sides are false.
+ //iii The result is true because both sides are true.
+ //iv The result is true because one of the sides are true.
+ //V it`s false becasue (!)
+ //VI it`s True because (!)
+ //VII it`s True because (!)
+ //VIII it`s false because (!)
+ //IX it`s True becasue (!)
+ //X it`s True becasue (!)
+ //Xi it`s False because There is (on) in both words.
+
+ //7
+ //i
+ console.log(now.getFullYear())
+ //ii
+ console.log(now.getMonth())
+ //iii
+ console.log(now.getDate())
+ ///iv
+ console.log(now.getDay())
+ //v
+ console.log(now.getHours())
+ //vi 
+ console.log(now.getMinutes())
+ //vii
+ console.log(now.getTime())
+
